4547a4f25c10290784ff87fe2d9c5735a15e4225
Og-Amayokoy <38034251+Og-Amayokoy@users.noreply.github.com>
2019-04-17 12:42:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+/* 
+To use "<script src="
 
+*/
+
+import defaultExport from "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js";
+
+$('input[name="zensou-bottom"]').change(function() {
+ document.getElementByClass("zensou").style.display ="none";
+}
