28fdc74f1791e682f6a764ba3cbdac34b6b509b3
Og-Amayokoy <38034251+Og-Amayokoy@users.noreply.github.com>
2019-04-17 13:14:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ To use "<script src="https://og-amayokoy.github.io/zenryaku-sousou/main.js" type
 */
 
 import defaultExport from "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js";
-
+/*
 $('input[name="zensou-bottom"]').change(function() {
  document.getElementByClass("zensou").style.display ="none";
+}
+
+          */                           
+                                    
+ fucnction ZensouMode() {
+var zensouModeValue =  document.zensou_bottom_form.zensou_bottom.checked;
+
+ if ( zensouModeValue == true){
+ document.getElementByClass("zensou").style.display ="none";
+ }
+ 
 }
