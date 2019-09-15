1c2812f7f1a08e730fc6ba02687fafde7f7fe1c3
Og-Amayokoy <38034251+Og-Amayokoy@users.noreply.github.com>
2019-09-15 19:50:59 +0900

    according to w3schoool how to js checkbox test

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ To use "<script src="https://og-amayokoy.github.io/zenryaku-sousou/main.js" type
 
 */
 
-import defaultExport from "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js";
 /*
 $('input[name="zensou-bottom"]').change(function() {
  document.getElementByClass("zensou").style.display ="none";
 }
 
           */                           
-                   
+
+          /*
  fucnction ZensouMode() {
 var zensouModeValue =  document.zensou_bottom_form.zensou_bottom.checked;
 
@@ -18,4 +18,23 @@ var zensouModeValue =  document.zensou_bottom_form.zensou_bottom.checked;
  document.getElementByClass("zensou").style.display ="none";
  }
  
+}
+
+
+*/
+
+document.getElementsByClassName("zensou").style.block;
+
+function zensou_clicked() {
+    var zensou_bottom = docment.getElementsById("zensou_bottom");
+
+    var none_and_block = document.getElementsByClassName("zensou")
+
+
+    if(zensou_bottom.checked == true){
+        Element.style.display="none";
+
+    }else{
+        Element.style.display="block";
+    }
 }
