d12745ffc0774b70d3ead6f7aeb5ddf7fbe966aa
Og-Amayokoy <38034251+Og-Amayokoy@users.noreply.github.com>
2019-09-15 19:58:50 +0900

    deleted lost

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
 /* 
-To use "<script src="https://og-amayokoy.github.io/zenryaku-sousou/main.js" type="text/javascript"></script>, you can get.
+Just add your html  tag
+"<script src="https://og-amayokoy.github.io/zenryaku-sousou/main.js" type="text/javascript"></script>
 
 */
 
-/*
-$('input[name="zensou-bottom"]').change(function() {
- document.getElementByClass("zensou").style.display ="none";
-}
-
-          */                           
-
-          /*
- fucnction ZensouMode() {
-var zensouModeValue =  document.zensou_bottom_form.zensou_bottom.checked;
-
- if ( zensouModeValue == true){
- document.getElementByClass("zensou").style.display ="none";
- }
- 
-}
-
-
-*/
-
-document.getElementsByClassName("zensou").style.block;
 
 function zensou_clicked() {
     var zensou_bottom = docment.getElementsById("zensou_bottom");
@@ -32,9 +12,9 @@ function zensou_clicked() {
 
 
     if(zensou_bottom.checked == true){
-        Element.style.display="none";
+        Text.style.display="none";
 
     }else{
-        Element.style.display="block";
+        text.style.display="block";
     }
 }
