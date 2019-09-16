759cea86cc8cee2f69e2fee05e2f703f7c9a8802
Og-Amayokoy <38034251+Og-Amayokoy@users.noreply.github.com>
2019-09-16 14:17:57 +0900

    help me  up plz

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ Just add your html  tag
 
 console.log ("js is runned")
 
-
 function myfucntion() {
     var zensou_area = document.getElementById("zensou_area");
-   /* var aaa = document.getElementsByClassName("zensou_area");*/
+
+    var zensou_area_class=document.getElementsByClassName("zensou_area_class")
+   
    var zensou_bottom=document.getElementById("zensou_bottom")
     console.log ("myfunction  is runned");
     if(zensou_bottom.checked == true){
         zensou_area.style.display="block";
+        zensou_area_class.style.display="block";
         console.log ("block")
     } else {
         zensou_area.style.display="none";
+        zensou_area_class.style.display="none";
         console.log ("none")
     }
 }
